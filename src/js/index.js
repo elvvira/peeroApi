23b985f0cb1a7945a618f190bfcb22024d1a5042
elvvira <elvvira.castellanos@gmail.com>
2023-03-20 23:09:08 +0100

    casa

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,25 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-// import catIMage from '../assets/images/cat.jpeg'
-// const img = document.createElement('img');
-// img.src = catImage;
-// document.body.append(img);
-import { sayHello } from './demo.js';
-sayHello();
+import { fetchData } from './fetch.js';
+
+const select = document.getElementById('select');
+
+const getListSelect = async () => {
+  const data = await fetchData('https://dog.ceo/api/breeds/list/all');
+  //   console.log(Object.values(data));
+  createOptions(Object.values(data.message));
+};
+getListSelect();
+
+const createOptions = async data => {
+  const fragment = document.createDocumentFragment();
+  const array = await data;
+  array.forEach(dog => {
+    console.dir(dog);
+    const optionElement = document.createElement('option');
+    optionElement.textContent = dog;
+    fragment.append(optionElement);
+  });
+  //   optionElement.textContent = data;
+
+  select.append(fragment);
+};
