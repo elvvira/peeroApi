5336c92cab218ff853b9a773ec24012d2eb979bf
elvvira <elvvira.castellanos@gmail.com>
2023-03-21 12:53:01 +0100

    casa

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,37 @@ import '../scss/styles.scss';
 import { fetchData } from './fetch.js';
 
 const select = document.getElementById('select');
+const image = document.getElementById('image');
+const button = document.getElementById('button');
+let imgFinal = '';
 
 const getListSelect = async () => {
   const data = await fetchData('https://dog.ceo/api/breeds/list/all');
-  //   console.log(Object.values(data));
-  createOptions(Object.values(data.message));
+  createOptions(data.message);
 };
 getListSelect();
-
-const createOptions = async data => {
+const createOptions = data => {
   const fragment = document.createDocumentFragment();
-  const array = await data;
-  array.forEach(dog => {
-    console.dir(dog);
+
+  Object.entries(data).forEach(([key]) => {
     const optionElement = document.createElement('option');
-    optionElement.textContent = dog;
+    optionElement.textContent = key;
     fragment.append(optionElement);
   });
-  //   optionElement.textContent = data;
-
   select.append(fragment);
 };
+
+const generateRandomImage = async value => {
+  const imgUrl = await fetchData(
+    `https://dog.ceo/api/breed/${value}/images/random`
+  );
+  imgFinal = imgUrl.message;
+};
+select.addEventListener('change', e => {
+  generateRandomImage(e.target.value);
+});
+
+button.addEventListener('click', () => {
+  generateRandomImage(select.value);
+  image.src = imgFinal;
+});
